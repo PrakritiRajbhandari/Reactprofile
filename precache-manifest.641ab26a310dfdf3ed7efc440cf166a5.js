8496c96ec4f4d919d5fad53da29cf98af3c1c584
Prakriti <rajbhandariprakriti.7@gmail.com>
2020-07-19 19:43:28 +1000

    Updates

---
+++ precache-manifest.641ab26a310dfdf3ed7efc440cf166a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9853938ce5b93788e515b1c2eef87401",
+    "url": "/Reactprofile/index.html"
+  },
+  {
+    "revision": "5ea155add7b175da8022",
+    "url": "/Reactprofile/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/Reactprofile/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactprofile/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea155add7b175da8022",
+    "url": "/Reactprofile/static/js/main.71a07a19.chunk.js"
+  },
+  {
+    "revision": "310903cbaaf50594f15e",
+    "url": "/Reactprofile/static/js/runtime-main.e83dbc0b.js"
+  }
+]);
